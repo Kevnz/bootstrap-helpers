483f93273a506e731c15bbd099d0761b7769a93b
Kevin Isom <kevin.isom@gmail.com>
2013-02-18 20:20:33 +1300

    added layouts and bootstrap files

---
+++ Bootstrap.Helpers.Samples/bootstrap.cs
@@ -0,0 +1,37 @@
+﻿using Nancy;
+using Nancy.Diagnostics;
+
+namespace Bootstrap.Helpers.Samples
+{
+
+
+
+	public class Bootstrapper : DefaultNancyBootstrapper
+	{
+		public Bootstrapper()
+		{
+			Cassette.Nancy.CassetteNancyStartup.OptimizeOutput = true;
+
+		}
+		protected override void ConfigureConventions(Nancy.Conventions.NancyConventions nancyConventions)
+		{
+			base.ConfigureConventions(nancyConventions);
+			nancyConventions.StaticContentsConventions.Add(Nancy.Conventions.StaticContentConventionBuilder.AddDirectory("/", "public"));
+
+			this.Conventions.AcceptHeaderCoercionConventions.Add((acceptHeaders, ctx) =>
+			{
+
+				// Modify the acceptHeaders by adding, removing or updating the current
+				// values.
+				var fb = "*/*";
+
+				return acceptHeaders;
+			});
+		}
+		protected override Nancy.Diagnostics.DiagnosticsConfiguration DiagnosticsConfiguration
+		{
+			get { return new DiagnosticsConfiguration { Password = @"nancy" }; }
+		}
+	}
+
+}
